6f6fcd9d2e794d909c9ba452a159599bc2d5aa54
lleiding <lleiding@users.noreply.github.com>
2017-02-20 14:54:33 +0100

    add init.el

---
+++ .emacs.d/init.el
@@ -0,0 +1,29 @@
+;;(add-to-list 'load-path "/home/ludwig/.emacs.d/themes/emacs-color-theme-solarized")
+;;(add-to-list 'custom-theme-load-path "/home/ludwig/.emacs.d/themes/emacs-color-theme-solarized/")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
+ '(package-selected-packages (quote (ess solarized-theme color-theme-solarized)))
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "DejaVu Sans Mono" :foundry "PfEd" :slant normal :weight normal :height 113 :width normal)))))
+;;(custom-set-faces (if (not window-system) '(default ((t (:background "nil"))))))
+(require 'package) ;; You might already have this line
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
+(when (< emacs-major-version 24)
+  ;; For important compatibility libraries like cl-lib
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+(package-initialize) ;; You might already have this line
+(xterm-mouse-mode t)
+(setq solarized-italic nil)    
+(load-theme 'solarized t)
