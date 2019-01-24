32187654c63d6735977759944311d3389eeb8920
Ludwig Leidinger <ludwig.leidinger@uni-wuerzburg.de>
2019-01-24 10:28:17 +0100

    attempt a general, more readable setup

--- init.el
+++ init.el
@@ -8,17 +8,19 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
+    ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
  '(frame-background-mode (quote light))
- '(package-selected-packages (quote (julia-repl ess color-theme-solarized)))
  '(menu-bar-mode nil)
+ '(package-selected-packages
+   (quote
+    (solarized-theme poly-R polymode julia-repl ess color-theme-solarized)))
  '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Fira Mono" :foundry "CTDB" :slant normal :weight normal :height 102 :width normal)))))
+ '(default ((t (:family "Fira Code" :foundry "CTDB" :slant normal :weight normal :height 102 :width normal)))))
 ;;(custom-set-faces (if (not window-system) '(default ((t (:background "nil"))))))
 (require 'package) ;; You might already have this line
 (add-to-list 'package-archives
@@ -28,6 +30,7 @@
   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
 (package-initialize) ;; You might already have this line
 (add-to-list 'load-path "/home/ludwig/.emacs.d/elpa/ess-20180216.1616/lisp/")
+(add-to-list 'auto-mode-alist '("\\.md" "\\.Rmd" . poly-markdown-mode))
 (load "ess-site")
 ;;(xterm-mouse-mode t)
 ;; (setq inferior-julia-program-name "/usr/bin/julia") ;; apparently not necessary
