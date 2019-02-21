404085aadb07cab8d5b184489ae0934b849f419e
Ludwig Leidinger <ludwig.leidinger@uni-wuerzburg.de>
2019-02-21 14:03:52 +0100

    polymode and turn off annoyances

--- init.el
+++ init.el
@@ -9,11 +9,12 @@
  '(custom-safe-themes
    (quote
     ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
+ '(ess-use-flymake nil)
  '(frame-background-mode (quote light))
  '(menu-bar-mode nil)
  '(package-selected-packages
    (quote
-    (solarized-theme poly-R polymode julia-repl ess color-theme-solarized)))
+    (poly-org solarized-theme poly-R polymode julia-repl ess color-theme-solarized)))
  '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -31,7 +32,9 @@
 (package-initialize) ;; You might already have this line
 (add-to-list 'load-path "/home/ludwig/.emacs.d/elpa/ess-20180216.1616/lisp/")
 (add-to-list 'auto-mode-alist '("\\.md" "\\.Rmd" . poly-markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.Rmd" . poly-markdown+r-mode))
 (load "ess-site")
+(ess-toggle-underscore nil)
 ;;(xterm-mouse-mode t)
 ;; (setq inferior-julia-program-name "/usr/bin/julia") ;; apparently not necessary
 ;; (setq solarized-italic nil)    
