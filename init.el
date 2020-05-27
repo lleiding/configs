b4bb7ba8d77dedb6523c4dc0b85c3e3bbd474654
Ludwig Leidinger <ludwig.leidinger@uni-wuerzburg.de>
2020-05-27 12:01:56 +0200

    fix File mode specification error and use not-base16 gruvbox theme

--- init.el
+++ init.el
@@ -4,16 +4,19 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-term-color-vector
+   [unspecified "#f9f5d7" "#9d0006" "#79740e" "#b57614" "#076678" "#8f3f71" "#076678" "#504945"] t)
  '(base16-highlight-mode-line (quote contrast))
  '(custom-safe-themes
    (quote
-    ("fee4e306d9070a55dce4d8e9d92d28bd9efe92625d2ba9d4d654fc9cd8113b7f" "b583823b9ee1573074e7cbfd63623fe844030d911e9279a7c8a5d16de7df0ed0" "0dd2666921bd4c651c7f8a724b3416e95228a13fca1aa27dc0022f4e023bf197" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
+    ("100eeb65d336e3d8f419c0f09170f9fd30f688849c5e60a801a1e6addd8216cb" "a06658a45f043cd95549d6845454ad1c1d6e24a99271676ae56157619952394a" "fee4e306d9070a55dce4d8e9d92d28bd9efe92625d2ba9d4d654fc9cd8113b7f" "b583823b9ee1573074e7cbfd63623fe844030d911e9279a7c8a5d16de7df0ed0" "0dd2666921bd4c651c7f8a724b3416e95228a13fca1aa27dc0022f4e023bf197" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
  '(ess-use-flymake nil)
+ '(fill-column 80)
  '(frame-background-mode nil)
  '(menu-bar-mode nil)
  '(package-selected-packages
    (quote
-    (base16-theme poly-org poly-R polymode julia-repl ess)))
+    (gruvbox-theme base16-theme poly-org poly-R polymode julia-repl ess)))
  '(show-paren-mode t)
  '(tool-bar-mode nil))
 (custom-set-faces
@@ -29,7 +32,9 @@
   ;; For important compatibility libraries like cl-lib
   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
 (package-initialize) ;; You might already have this line
-(add-to-list 'auto-mode-alist '("\\.md" "\\.Rmd" . poly-markdown-mode))
+(require 'poly-markdown)
+(add-to-list 'auto-mode-alist '("\\.md" . poly-markdown-mode))
+(require 'poly-R)
 (add-to-list 'auto-mode-alist '("\\.Rmd" . poly-markdown+r-mode))
 (load "ess-site")
 (ess-toggle-underscore nil)
